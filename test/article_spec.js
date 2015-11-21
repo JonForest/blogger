8c57b975f1576acccdb12c46b970e25b79fd0505
Jon Hollingsworth <jonforest@gmail.com>
2015-11-22 09:50:04 +1300

    Added initial test definitions

---
+++ test/article_spec.js
@@ -0,0 +1,12 @@
+/* global describe, it */
+
+describe('Articles', function() {
+    it('can add a new article');
+    it('can retrieve a new article for editing');
+    it('new article can be edited');
+    it('saved article is immediately published');
+    it('article can be published');
+    it('published article can be updated, with changes not appearing on published article');
+    it('changes to published article are only visible when "re-publish" is selected');
+    it('articles can be deleted');
+});
---
+++ test/ui_spec.js
@@ -0,0 +1,6 @@
+describe('UI behaviour', function() {
+    it('shows the text "no articles publish" before any articles published');
+    it('shows a list of published articles and their publish date');
+    it('updated articles still show the initial publish data');
+    it('article can be loaded with own static url');
+});
